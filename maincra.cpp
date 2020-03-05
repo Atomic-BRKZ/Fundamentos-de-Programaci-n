4db2629f366fb170bf5f527515cef033d59ca3a3
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-03-05 10:01:01 -0600

    Update maincra.cpp

--- maincra.cpp
+++ maincra.cpp
@@ -1,12 +1,12 @@
-# include  < stdio.h >
+# include  <stdio.h>
 
-# define  TAMA—O  1000
+# define  TAMANO  1000
 
 int  main () {
   int aguante, num_filas;
-  long  long  int diamantes [TAMA—O];
-  largo  largo  int suma1 = 0 , suma2 = 0 ;
-  
+  long  long  int diamantes [TAMANO];
+  long  long  int suma1 = 0 , suma2 = 0 ;
+  printf("Ingrese el numero de filas: \n");
   scanf ( " % d " , & num_filas); // se pide numero de filas
 
   printf ( " Ingrese los diamantes en cada fila " );
@@ -15,24 +15,24 @@ int  main () {
     scanf ( " % lld " , & diamantes [i]);
   }
   printf ( " Ingrese el aguante del pico " );
-  scanf ( " % d " , y aguante); // se pide el aguante del pico
+  scanf ( " % d " , & aguante); // se pide el aguante del pico
 
 
   for ( long  long  int i = 0 ; i <num_filas; i ++) {
-        if ((num_filas - i) <aguante) continuar ;
+        if ((num_filas - i) <aguante) continue ;
         for ( long  long  int   j = 0 ; j <aguante; j ++) {
-            suma1 + = diamantes [i + j];
+            suma1 += diamantes [i + j];
         }
-        if (suma1> = suma2) {
+        if (suma1>= suma2) {
             suma2 = suma1;
             suma1 = 0 ;
         }
-        m·s {
+         {
             suma1 = 0 ;
         }
     }
 
 
-  printf ( " \ n \ n % lld \ n \ n " , suma2); // se imprime el valor m·s alto
-  devuelve  0 ;
+  printf ( "\n \n %lld \n \n " , suma2); // se imprime el valor m√°s alto
+  return  0 ;
 }
