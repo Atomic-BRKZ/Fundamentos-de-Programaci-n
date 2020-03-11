12e57c8be5a90141502b0618610f5ac5bdd36c49
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-03-11 07:44:09 -0600

    Update programa 59 palindromo.c

--- programa 59 palindromo.c
+++ programa 59 palindromo.c
@@ -1,40 +1,26 @@
-
 #include <stdio.h>
-#include <conio.h>
-#include <string.h>
-//===============================================================================
-#define TAM 6
-//===============================================================================
-//   ESTRUCTURAS
-//===============================================================================
-//===============================================================================
-//   PROTOTIPO DE FUNCIONES
-//===============================================================================
-void encabezado(void);
-void fin(void);
-//===============================================================================
-//   PRINCIPAL
-//===============================================================================
-int main(void){
-	char cadena[TAM];
-	int contador;
-	int a;
-	int b;
-	system("cls");
-	system("color 71");
-	encabezado();
-	printf("\n\t\t\tIngresa una palabra: ");
-	scanf("%s", &cadena);
-    a=strlen(cadena);
-    a=a-1;
-    for(contador=0, b=a; contador<=a/2; contador++, b--);
-    	if(cadena[contador]==cadena[b]){
-    		printf("\n\n\t\t\tEs un palindromo.\n");
-		}else{
-			printf("\n\n\t\t\tNo es un palindromo.\n");
-		}
-	fin();
-	getch();
-	return 0;
+#include <stdlib.h>
+
+int main()
+{
+    char i,j;
+char longitud;
+char cadena[50];
+char temporal;
+printf("Introduce un texto (menos de 50 caracteres): ");
+gets(cadena);
+longitud=strlen(cadena);
+for (i=0,j=longitud-1; i<longitud/2; i++,j--)
+{
+	temporal=cadena[i];
+	cadena[i]=cadena[j];
+	cadena[j]=temporal;
+}
+printf("%s",cadena);
+
+
+
+
+    return 0;
 }
 
